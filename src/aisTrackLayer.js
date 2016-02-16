3c19c83288db2c085737e03c26f7926987476020
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-02-16 13:46:37 +0100

    set RemoveTime

--- src/aisTrackLayer.js
+++ src/aisTrackLayer.js
@@ -7,6 +7,7 @@ L.AISTrackLayer = L.FeatureGroup.extend({
     initialize: function(layers){
         L.LayerGroup.prototype.initialize.call(this, layers);
         this._intervalDeadObjs = setInterval(this._checkDeadObjects,1000,this);
+        this.setRemoveTime(6);
     },
 
     addAisData: function(mmsi,data){
