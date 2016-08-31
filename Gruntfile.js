d224ddbeea136218e337df2df90d2e5da77f68db
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-08-31 10:45:00 +0200

    add Grunt File

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+/* global module */
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+          dist: {
+            src: ['src/*.js','src/label/*.js'],
+            dest: 'dist/<%= pkg.name %>.js'
+          }
+        },
+
+        jshint: {
+            all: ['dist/<%= pkg.name %>.js']
+        },
+
+        cssmin:{
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'dist/<%= pkg.name %>.css': ['src/*.css','src/label/*.css']
+                }
+            }
+        },
+
+        csslint: {
+            strict: {
+                src: ['src/*.css','src/label/*.css']
+            },
+        },
+
+        
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> Copyright by <%= pkg.author.name %> <%= pkg.author.email %> */\n'
+          },
+          build: {
+            src: 'dist/<%= pkg.name %>.js',
+            dest: 'dist/<%= pkg.name %>.min.js'
+          }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-yuidoc');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('check', ['jshint', 'csslint']);
+    grunt.registerTask('test', ['qunit']);
+    grunt.registerTask('jenkins', ['jshint', 'qunit']);
+    grunt.registerTask('default', ['concat', 'csslint', 'jshint', 'uglify', 'cssmin']);
+};
