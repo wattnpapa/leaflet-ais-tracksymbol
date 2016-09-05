be101d0af1e965a14cd6c49de76386bcae1cae6d
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-09-05 11:52:08 +0200

    update dist

--- dist/leaflet-ais-tracksymbol.js
+++ dist/leaflet-ais-tracksymbol.js
@@ -2143,3 +2143,380 @@ L.Path.include({
 		this.tracksymbollabel.close();
 	}
 });
+
+L.TrackLayer = function() {
+  var aislayer = L.featureGroup([]);
+
+  this.addTrack = function(marker) {
+    marker.addTo(aislayer);
+  };
+
+  this.removeTrack = function(marker) {
+    marker.removeLayer(aislayer);
+  };
+
+
+  this.addTo = function(map) {
+    aislayer.addTo(map);
+  };
+};
+
+L.trackLayer = function() {
+  return new L.TrackLayer();
+};
+
+
+/**
+ * Tracksymbol for leaflet.
+ * The visualization is chosen by zoomlevel or heading availability.
+ * If zoomlevel is smaller than 'minSilouetteZoom' a triangular symbol is rendered.
+ * If zoomlevel is greater than 'minSilouetteZoom' a ship silouette is rendered.
+ * If heading is undefined a diamond symbol is rendered.
+ * The following options are available:
+ * <ul>
+ *   <li>trackId: The unique id of the symbol (default: 0). </li>
+ *   <li>size: Static size of the symbol in pixels (default:24). </li>
+ *   <li>heading: Initial heading of the symbol (default: undefined). </li>
+ *   <li>course: Initial course of the symbol (default: undefined). </li>
+ *   <li>speed: Initial speed of the symbol-leader (default: undefined). </li>
+ *   <li>leaderTime: The length of the leader (speed * leaderTime) (default:60s). </li>
+ *   <li>minSilouetteZoom: The zoomlevel to switch from triangle to silouette (default:14). </li>
+ *   <li>gpsRefPos: Initial GPS offset of the symbol (default: undefined). </li>
+ *   <li>defaultSymbol: The triangular track symbol. Contains an array of n numbers. [x1,y1,x2,y2,...] </li>
+ *   <li>noHeadingSymbol: The diamond track symbol. Contains an array of n numbers. [x1,y1,x2,y2,...] </li>
+ *   <li>silouetteSymbol: The ship track symbol. Contains an array of n numbers. [x1,y1,x2,y2,...] </li>
+ * </ul>
+ * @class TrackSymbol
+ * @constructor
+ * @param latlng {LanLng} The initial position of the symbol.
+ * @param options {Object} The initial options described above.
+ */
+L.TrackSymbol = L.Path.extend({
+
+  initialize: function (latlng, options) {
+    L.Path.prototype.initialize.call(this, options);
+    if(latlng === undefined) {
+      throw Error('Please give a valid lat/lon-position');
+    }
+    options = options || {};
+    this._id = options.trackId || 0;
+    this._leaflet_id = this._id; 
+    this._latlng = L.latLng(latlng);
+    this._size = options.size || 24;
+    this._heading = options.heading;
+    this._course = options.course;
+    this._speed = options.speed;
+    this._leaderTime = options.leaderTime || 60.0;
+    this._minSilouetteZoom = options.minSilouetteZoom || 14;
+    this.setGPSRefPos(options.gpsRefPos);
+    this._triSymbol = options.defaultSymbol || [0.75,0, -0.25,0.3, -0.25,-0.3];
+    this._diaSymbol = options.noHeadingSymbol || [0.3,0, 0,0.3, -0.3,0, 0,-0.3];
+    this._silSymbol = options.silouetteSymbol || [1,0.5, 0.75,1, 0,1, 0,0, 0.75,0];
+  },
+
+  /**
+   * Set latitude/longitude on the symbol.
+   * @method setLatLng
+   * @param latlng {LatLng} Position of the symbol on the map. 
+   */
+  setLatLng: function (latlng) {
+    this._latlng = L.latLng(latlng);
+    return this.redraw();
+  },
+  
+  /**
+   * Set the speed shown in the symbol [m/s].
+   * The leader-length is calculated via leaderTime.
+   * @method setSpeed
+   * @param speed {Number} The speed in [m/s]. 
+   */
+  setSpeed: function( speed ) {
+    this._speed = speed;
+    return this.redraw();
+  },
+  
+  /**
+   * Sets the course over ground [rad].
+   * The speed-leader points in this direction.
+   * @method setCourse
+   * @param course {Number} The course in radians.
+   */
+  setCourse: function( course ) {
+    this._course = course;
+    return this.redraw();
+  },
+  
+  /**
+   * Sets the heading of the symbol [rad].
+   * The heading rotates the symbol.
+   * @method setHeading
+   * @param course {Number} The heading in radians.
+   */
+  setHeading: function( heading ) {
+    this._heading = heading;
+    return this.redraw();
+  },
+  
+  /**
+   * Sets the leaderTime of the symbol [seconds].
+   * @method setLeaderTime
+   * @param leaderTime {Number} The leaderTime in seconds.
+   */
+  setLeaderTime: function( leaderTime ) {
+    this._leaderTime = leaderTime;
+    return this.redraw();
+  },
+
+  /**
+   * Sets the position offset of the silouette to the center of the symbol.
+   * The array contains the refpoints from ITU R-REC-M.1371-4-201004 page 108
+   * in sequence A,B,C,D.
+   * @method setGPSRefPos
+   * @param gpsRefPos {Array} The GPS offset from center.
+   */
+  setGPSRefPos: function(gpsRefPos) {
+    if(gpsRefPos === undefined || 
+       gpsRefPos.length < 4) {
+      this._gpsRefPos = undefined;
+    }
+    else if(gpsRefPos[0] === 0 && 
+            gpsRefPos[1] === 0 && 
+            gpsRefPos[2] === 0 && 
+            gpsRefPos[3] === 0) {
+      this._gpsRefPos = undefined;
+    }
+    else {
+      this._gpsRefPos = gpsRefPos;
+    }
+    return this.redraw();
+  },
+
+  /**
+   * Returns the trackId.
+   * @method getTrackId
+   * @return {Number} The track id.
+   */
+  getTrackId: function() {
+    return this._trackId;
+  },
+    
+  _getLatSize: function () {
+    return this._getLatSizeOf(this._size);
+  },
+
+  _getLngSize: function () {
+    return this._getLngSizeOf(this._size);
+  },
+  
+  _getLatSizeOf: function (value) {
+    return (value / 40075017) * 360;
+  },
+
+  _getLngSizeOf: function (value) {
+    return ((value / 40075017) * 360) / Math.cos(L.LatLng.DEG_TO_RAD * this._latlng.lat);
+  },
+
+  /**
+   * Returns the bounding box of the symbol.
+   * @method getBounds
+   * @return {LatLngBounds} The bounding box.
+   */
+  getBounds: function () {
+     var lngSize = this._getLngSize() / 2.0;
+     var latSize = this._getLatSize() / 2.0;
+     var latlng = this._latlng;
+     return new L.LatLngBounds(
+            [latlng.lat - latSize, latlng.lng - lngSize],
+            [latlng.lat + latSize, latlng.lng + lngSize]);
+  },
+
+  /**
+   * Returns the position of the symbol on the map.
+   * @mathod getLatLng
+   * @return {LatLng} The position object.
+   */
+  getLatLng: function () {
+    return this._latlng;
+  },
+
+  //
+  // Rotates the given point around the angle.
+  // @method _rotate
+  // @param point {Array} A point vector 2d.
+  // @param angle {Number} Angle for rotation [rad].
+  // @return The rotated vector 2d.
+  //
+  _rotate: function(point, angle) {
+    var x = point[0];
+    var y = point[1];
+    var si_z = Math.sin(angle);
+    var co_z = Math.cos(angle);
+    var newX = x * co_z - y * si_z;
+    var newY = x * si_z + y * co_z;
+    return [newX, newY];
+  },
+
+  //
+  // Rotates the given point-array around the angle.
+  // @method _rotateAllPoints
+  // @param points {Array} A point vector 2d.
+  // @param angle {Number} Angle for rotation [rad].
+  // @return The rotated vector-array 2d.
+  //
+  _rotateAllPoints: function(points, angle) {
+    var result = [];
+    for(var i=0;i<points.length;i+=2) {
+      var x = points[i + 0] * this._size;
+      var y = points[i + 1] * this._size;
+      var pt = this._rotate([x, y], angle);
+      result.push(pt[0]);
+      result.push(pt[1]);
+    }
+    return result;
+  },
+
+  _createLeaderViewPoints: function(angle) {
+    var result = [];
+    var leaderLength = this._speed * this._leaderTime;
+    var leaderEndLng = this._latlng.lng + this._getLngSizeOf(leaderLength * Math.cos(angle));
+    var leaderEndLat = this._latlng.lat + this._getLatSizeOf(leaderLength * Math.sin(angle));
+    var endPoint = this._map.latLngToLayerPoint(L.latLng([leaderEndLat, leaderEndLng]));
+    var startPoint = this._map.latLngToLayerPoint(this._latlng);
+    return [startPoint.x, startPoint.y, endPoint.x, endPoint.y];
+  },
+
+  _transformAllPointsToView: function(points) {
+    var result = [];
+    var symbolViewCenter = this._map.latLngToLayerPoint(this._latlng);
+    for(var i=0;i<points.length;i+=2) {
+      var x = symbolViewCenter.x + points[i+0];
+      var y = symbolViewCenter.y - points[i+1];
+      result.push(x);
+      result.push(y);
+    }
+    return result;
+  },
+
+  _createPathFromPoints: function(points) {
+    var result;
+    for(var i=0;i<points.length;i+=2) {
+      var x = points[i+0];
+      var y = points[i+1];
+      if(result === undefined)
+        result = 'M ' + x + ' ' + y + ' ';
+      else
+        result += 'L ' + x + ' ' + y + ' ';
+    }
+    return result + ' Z';
+  },
+
+  _getViewAngleFromModel:  function(modelAngle) {
+    return Math.PI/2.0 - modelAngle;
+  },
+
+  _createNoHeadingSymbolPathString: function() {
+    var viewPoints = this._transformAllPointsToView( this._rotateAllPoints(this._diaSymbol, 0.0) );
+    var viewPath = this._createPathFromPoints(viewPoints);
+    if( this._course !== undefined && this._speed !== undefined ) {
+      var courseAngle = this._getViewAngleFromModel(this._course);
+      var leaderPoints = this._createLeaderViewPoints(courseAngle);
+      viewPath += '' + this._createPathFromPoints(leaderPoints);
+    }
+    return viewPath;
+  },
+
+  _createWithHeadingSymbolPathString: function() {
+    var headingAngle = this._getViewAngleFromModel(this._heading);
+    var viewPoints = this._transformAllPointsToView( this._rotateAllPoints(this._triSymbol, headingAngle) );
+    var viewPath = this._createPathFromPoints(viewPoints);
+    if( this._course !== undefined && this._speed !== undefined ) {
+      var courseAngle = this._getViewAngleFromModel(this._course);
+      var leaderPoints = this._createLeaderViewPoints(courseAngle);
+      viewPath += '' + this._createPathFromPoints(leaderPoints);
+    }
+    return viewPath;
+  },
+
+  _resizeAndMovePoint: function(point, size, offset) {
+    return [
+      point[0] * size[0] + offset[0], 
+      point[1] * size[1] + offset[1]
+    ];
+  },
+
+  _getSizeFromGPSRefPos: function() {
+    return [
+      this._gpsRefPos[0] + this._gpsRefPos[1],
+      this._gpsRefPos[2] + this._gpsRefPos[3]
+    ];
+  },
+
+  _getOffsetFromGPSRefPos: function() {
+    return [
+      -this._gpsRefPos[1], 
+      -this._gpsRefPos[3]
+    ];
+  },
+
+  _transformSilouetteSymbol: function() {
+    var headingAngle = this._getViewAngleFromModel(this._heading);
+    var result = [];
+    var size = this._getSizeFromGPSRefPos();
+    var offset = this._getOffsetFromGPSRefPos();
+    for(var i=0;i<this._silSymbol.length;i+=2) {
+      var pt = [
+        this._silSymbol[i+0], 
+        this._silSymbol[i+1]
+      ];
+      pt = this._resizeAndMovePoint(pt, size, offset);
+      pt = this._rotate(pt, headingAngle);
+      var pointLng = this._latlng.lng + this._getLngSizeOf(pt[0]);
+      var pointLat = this._latlng.lat + this._getLatSizeOf(pt[1]);
+      var viewPoint = this._map.latLngToLayerPoint(L.latLng([pointLat, pointLng]));
+      result.push(viewPoint.x);
+      result.push(viewPoint.y);
+    }
+    return result;
+  },
+
+  _createSilouetteSymbolPathString: function() {
+    var silouettePoints = this._transformSilouetteSymbol();
+    var viewPath = this._createPathFromPoints(silouettePoints);
+    if( this._course !== undefined && this._speed !== undefined ) {
+      var courseAngle = this._getViewAngleFromModel(this._course);
+      var leaderPoints = this._createLeaderViewPoints(courseAngle);
+      viewPath += '' + this._createPathFromPoints(leaderPoints);
+    }
+    return viewPath;
+  },
+
+  /**
+   * Generates the symbol as SVG path string.
+   * depending on zoomlevel or track heading different symbol types are generated.
+   * @return {String} The symbol path string.
+   */
+  getPathString: function () {
+    if(this._heading === undefined) {
+      return this._createNoHeadingSymbolPathString();
+    }
+    else {
+      if(this._gpsRefPos === undefined || this._map.getZoom() <= this._minSilouetteZoom ) {
+        return this._createWithHeadingSymbolPathString();
+      }
+      else {
+        return this._createSilouetteSymbolPathString();
+      }
+    }
+  }
+});
+
+/**
+ * Factory function to create the symbol.
+ * @method trackSymbol
+ * @param latlng {LatLng} The position on the map.
+ * @param options {Object} Additional options. 
+ */
+L.trackSymbol = function (latlng, options) {
+    return new L.TrackSymbol(latlng, options);
+};
+
