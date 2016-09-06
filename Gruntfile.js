3d3832ed54bff86487929e2ccef486f00e80cf12
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-09-06 09:25:17 +0200

    change order

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
 
         concat: {
           dist: {
-            src: ['src/*.js','src/label/*.js','node_modules/leaflet-tracksymbol/leaflet-tracklayer.js','node_modules/leaflet-tracksymbol/leaflet-tracksymbol.js'],
+            src: ['node_modules/leaflet-tracksymbol/leaflet-tracklayer.js','node_modules/leaflet-tracksymbol/leaflet-tracksymbol.js','src/*.js','src/label/*.js'],
             dest: 'dist/<%= pkg.name %>.js'
           }
         },
