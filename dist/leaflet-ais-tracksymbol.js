06bd38aafa3ba8150ff411ccf8b93cff1950c373
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-09-09 12:20:34 +0200

    v1.1.3

--- dist/leaflet-ais-tracksymbol.js
+++ dist/leaflet-ais-tracksymbol.js
@@ -1195,7 +1195,9 @@ L.AISTrackSymbol = L.TrackSymbol.extend({
      * @returns {string}
      */
     getUTCTime: function(){
-       return this.getUTCYear() + "." + this.getUTCMonth() + "." + this.getUTCDay() + " " + this.getUTCHour() + ":" + this.getUTCMinute() + ":" + this.getUTCSecond() + " UTC";
+        if (typeof getUTCYear() !== 'undefined')
+            return null;
+        return this.getUTCYear() + "." + this.getUTCMonth() + "." + this.getUTCDay() + " " + this.getUTCHour() + ":" + this.getUTCMinute() + ":" + this.getUTCSecond() + " UTC";
     },
 
     /**
@@ -1211,7 +1213,7 @@ L.AISTrackSymbol = L.TrackSymbol.extend({
      * @returns {*}
      */
     getTypeOfDeviceText: function(){
-        this._setColorByTypeOfDevice();
+        //this._setColorByTypeOfDevice();
         switch (this.getTypeOfDevice()){
             case 0:
                 return "undefined (default)";
